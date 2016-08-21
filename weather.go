99527032ea631af00bc2b22d9b81eb2d2864b860
John Kisch <jkisch21@gmail.com>
2016-08-21 02:06:48 -0400

    added cli args and error handling

--- weather.go
+++ weather.go
@@ -1,15 +1,12 @@
 // This code fetches weather conditions from the weather undergroud API
 // to be displayed in the i3 status bar.
 
-// You can edit this code!
-// Click here and start typing.
 
 package main
 
 import "github.com/Jeffail/gabs"
 import "fmt"
 import "os"
-import "encoding/json"
 import "net/http"
 import "io/ioutil"
 
@@ -26,25 +23,37 @@ type Weather struct {
 }
 
 func main() {
+    // select a place from which to query weather
+    city  := os.Args[1]
+    state := os.Args[2]
+
     // make HTTP GET request
-    resp, err := http.Get("http://api.wunderground.com/api/a771a3d22176fe4f/conditions/q/CA/San_Francisco.json")
+    resp, err := http.Get("http://api.wunderground.com/api/a771a3d22176fe4f/conditions/q/"+ state +"/"+ city + ".json")
+
     // if there is an error call our error handling function
     if err != nil { do_err() }
+
     // close our HTTP connection
     defer resp.Body.Close()
     body, err := ioutil.ReadAll(resp.Body)
     if err != nil {do_err() }
-    fmt.Printf("%s", body)
+    //fmt.Printf("%s", body)
 
+    // parse our JSON from wunderground API
     jsonParsed, err := gabs.ParseJSON(body)
 
-    var conditions Weather
+    invalid := jsonParsed.ExistsP("response.error.type")
+    if invalid{
+        do_err()
+    } else{
+        var conditions Weather
 
-    conditions.weather = jsonParsed.Path("current_observation.weather").Data().(string)
-    conditions.temp_f = jsonParsed.Path("current_observation.temp_f").Data().(float64)
-    conditions.temp_c = jsonParsed.Path("current_observation.temp_c").Data().(float64)
+        conditions.weather = jsonParsed.Path("current_observation.weather").Data().(string)
+        conditions.temp_f = jsonParsed.Path("current_observation.temp_f").Data().(float64)
+        conditions.temp_c = jsonParsed.Path("current_observation.temp_c").Data().(float64)
 
-    fmr.println(conditions)
+        fmt.Println(conditions)
+    }
 
 
 }
